ca6eb2165f79399801b46164b2c08e689b49070b
CoreshiftD <vinusdiki@gmail.com>
2026-04-25 19:45:30 +0000

    rust(runtime): consume lowlevel 0.2 preview

--- rust/src/runtime/signals.rs
+++ rust/src/runtime/signals.rs
@@ -1,10 +1,8 @@
-use coreshift_lowlevel::sys::{SignalRuntime, install_shutdown_flag};
+use coreshift_lowlevel::sys::install_shutdown_flag;
 use std::sync::atomic::AtomicBool;
 
 pub static SHUTDOWN: AtomicBool = AtomicBool::new(false);
 
 pub fn setup() {
-    // Unblock all signals
-    let _ = SignalRuntime::unblock_all();
     let _ = install_shutdown_flag(&SHUTDOWN);
 }
